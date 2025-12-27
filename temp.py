5ad77b41b4c8358a0b72c45a9f17d1adc6bcee7a
Ravada Satyadev <119041370+satyadevravada@users.noreply.github.com>
2025-12-27 22:55:26 +0530

    Delete temp.py

--- temp.py
+++
@@ -1,4 +0,0 @@
-import torch
-print(torch.cuda.is_available())       # Should be True
-print(torch.cuda.current_device())     # Index of the GPU
-print(torch.cuda.get_device_name(0))   # Name of your GPU
